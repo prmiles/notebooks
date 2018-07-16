43a4012cf323310b47f87f6677e225ea70dbc9cb
prmiles <prmiles@ncsu.edu>
2018-07-16 17:49:27 -0400

    removed extraneous comments from file

--- pymcmcstat/viscoelasticity/linear_viscoelastic_model.cpp
+++ pymcmcstat/viscoelasticity/linear_viscoelastic_model.cpp
@@ -16,22 +16,3 @@ for (int ii = 0; ii < nx; ii++){
     }
 return q;
 }
-
-
-// def linear_viscoelastic_model(theta, stretch, time):
-//    # unpack model parameters
-//#    eta = theta['eta']
-//#    gamma = theta['gamma']
-//#    
-//#    tau = eta/gamma # viscoelastic time constant
-//#    
-//#    dt = np.ones([stretch.size]) # time step
-//#    dt[1:] = time[1:]-time[0:-1]
-//#    n = stretch.size
-//#    q = np.zeros([n,1])
-//#    for kk in range(1,n):
-//#        Tnc = 1 - dt[kk]/(2*tau);
-//#        Tpc = 1 + dt[kk]/(2*tau);
-//#        Tpcinv = Tpc**(-1);
-//#        q[kk] = Tpcinv*(Tnc*q[kk-1] + gamma*(stretch[kk] - stretch[kk-1]));
-//#    return q
